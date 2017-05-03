f6c6fd6a3ad55e844df4a977d1995b70cc2b4b82
AlexSchizoid <AlexSchizoid@users.noreply.github.com>
2017-05-03 18:35:21 +0300

    Create config.sh
    initial

---
+++ config.sh
@@ -0,0 +1,9 @@
+git clone https://github.com/udacity/CarND-LeNet-Lab.git
+git clone https://github.com/AlexSchizoid/CarND-Traffic-Sign-Classifier-Project.git
+cd CarND-Traffic-Sign-Classifier-Project
+mkdir data
+cd data
+wget https://d17h27t6h515a5.cloudfront.net/topher/2017/February/5898cd6f_traffic-signs-data/traffic-signs-data.zip
+unzip traffic-signs-data.zip
+cd
+source activate carnd-term1
